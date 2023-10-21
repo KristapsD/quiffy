0212df2c9164befaf556180100e9bed872701d32
KristapsD <kristapsd2033@gmail.com>
2023-10-21 17:07:24 +0300

    Moved from xhr2 to fetch. made functions ASYNC. added getstream(target) method. added dispose() method. TODO Run initialize in constructor

--- Code/TwitchAuth.js
+++ Code/TwitchAuth.js
@@ -1,30 +1,76 @@
-var XMLHttpRequest = require('xhr2');
-
 class TwitchAuth {
     constructor(clientId, clientSecret, redirectUri = "") {
         this.clientId = clientId;
         this.clientSecret = clientSecret;
         this.redirectUri = redirectUri;
+        this.access_token = undefined;
     }
 
-    authorize() {
-        const data = `client_id=${this.clientId}&client_secret=${this.clientSecret}&grant_type=client_credentials`;
+    async initialize() {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+        
+        var urlencoded = new URLSearchParams();
+        urlencoded.append("client_id", this.clientId);
+        urlencoded.append("client_secret", this.clientSecret);
+        urlencoded.append("grant_type", "client_credentials");
+        
+        var requestOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          body: urlencoded,
+          redirect: 'follow'
+        };
+        
+        return await fetch("https://id.twitch.tv/oauth2/token", requestOptions)
+          .then(response => response.json())
+          .then((result) => {
+            this.access_token = result.access_token;
+            return result.access_token;
+           })
+          .catch(error => console.log('error', error));
+    }
 
-        let xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
+    async getStream(streamLogin) {
+        var myHeaders = new Headers();
+        myHeaders.append("Client-Id", this.clientId);
+        myHeaders.append("Authorization", `Bearer ${this.access_token}`);
 
-        xhr.addEventListener("readystatechange", function() {
-        if(this.readyState === 4) {
-            console.log(this.responseText);
-        }
-        });
+        var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+        };
 
-        xhr.open("POST", "https://id.twitch.tv/oauth2/token");
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        return await fetch(`https://api.twitch.tv/helix/streams?user_login=${streamLogin}&type=all`, requestOptions)
+        .then(response => response.json())
+        .then((result) => {
+            return result.data;
+        })
+        .catch(error => console.log('error', error));
+    }
 
-        xhr.send(data);
-
-        return data.access_token;
+    async dispose() {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+        
+        var urlencoded = new URLSearchParams();
+        urlencoded.append("client_id", this.clientId);
+        urlencoded.append("token", this.access_token);
+        
+        var requestOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          body: urlencoded,
+          redirect: 'follow'
+        };
+        
+        return await fetch("https://id.twitch.tv/oauth2/revoke", requestOptions)
+          .then((response) =>  {
+            if(response.status === 200)
+                this.access_token = undefined;
+            })
+          .catch(error => console.log('error', error));
     }
 }
 
