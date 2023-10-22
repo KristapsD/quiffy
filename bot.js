d52251c9fb8b02897927e782dc6a5038aa309a54
KristapsD <kristapsd2033@gmail.com>
2023-10-22 18:40:15 +0300

    More directions for pyramid checking. and stream live logic: Check every 15 mins, if stream is live, and if the bot is connected. do the needed action according to the information

--- bot.js
+++ bot.js
@@ -1,11 +1,18 @@
 const botLogic = require('./Code/botLogic.js');
 const TwitchApi = require('./Code/TwitchApi.js');
 const responsesArray = require('./Code/Store/Responses.json');
+const allEqual = arr => arr.every(val => val === arr[0]);
 const EnvVars = {};
+let connected = false;
+let last5MessageSenders = [];
+let last5MessagesMaybePyramid = {
+    "pyramid": false,
+    "count": 0
+};
 
 require('dotenv').config({ path: "Environments/local.env", processEnv: EnvVars });
 
-const bot = new botLogic(EnvVars, ['quifenadine']);
+const bot = new botLogic(EnvVars, ['elajjaz']);
 const client = bot.start();
 
 // Register our event handlers (defined below)
@@ -15,29 +22,72 @@ client.on('connected', onConnectedHandler);
 // Connect to Twitch:
 client.connect();
 
+setInterval(async function() {
+    //check if stream is live every 15 mins, if live disconnect, otherwise connect
+    const streamLive = await isStreamLive('elajjaz');
+
+    console.log(new Date());
+    console.log(`Am i connected: ${connected}.`);
+    console.log(`Is the stream live: ${streamLive}.`);
+
+    if(streamLive && connected) {
+        console.log("I think the stream is live, and i am connected, disconnecting.")
+        client.disconnect();
+    } else if (!streamLive && !connected) {
+        console.log("I think the stream is not live, and i am not connected, connecting.")
+        client.connect();
+    }
+}, 15 * 60 * 1000);
+
 // Called every time a message comes in
 async function onMessageHandler (target, context, msg, self) {
+    const streamLive = await isStreamLive(target.replace('#', '')); // double check stream live
+    if (streamLive) { return; }; //ignore if channel is live
+
+    //add everyone to the last 5 senders array
+    last5MessageSenders.push(context['user-id']);
+
+    if(last5MessageSenders.length > 5)
+        last5MessageSenders.shift();
+
     if (self) { return; } // Ignore messages from the bot
-    if (!isStreamLive(target.replace('#', ''))) { return; }; //ignore if channel is live
+
+    let response = responsesArray[getRandomInt(responsesArray.length)];
+    const isPyramid = await checkPyramid(msg);
+
+    if(isPyramid) {
+        last5MessagesMaybePyramid["pyramid"] = true;
+        last5MessagesMaybePyramid["count"] = 5;
+    } else if(last5MessagesMaybePyramid["count"] >= 0) {
+        last5MessagesMaybePyramid["count"] = last5MessagesMaybePyramid["count"] - 1;
+        if(last5MessagesMaybePyramid["count"] <= 0) {
+            last5MessagesMaybePyramid["pyramid"] = false;
+        }
+    }
 
     console.log(context['user-id']);
-    //me or awakenedgarou
-    if(context['user-id'].toLowerCase() === '135624254' || context['user-id'].toLowerCase() === '63633617') {
-        if(await checkPyramid(msg)){
+    console.log(last5MessagesMaybePyramid)
 
-            client.say(target, responsesArray[getRandomInt(20)]).catch((err)=> {console.log(err)});
+    if(last5MessageSenders.length === 5 && allEqual(last5MessageSenders) && last5MessageSenders[0] === "63633617"){
+        if(last5MessagesMaybePyramid["pyramid"]) {
+            console.log(response)
+            client.say(target, response).catch((err)=> {console.log(err)});
         }
+    }
+
+    if(last5MessageSenders.length === 5 && isPyramid && allEqual(last5MessageSenders)){
+        console.log(response)
+        client.say(target, response).catch((err)=> {console.log(err)});
     }
 }
 
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
     console.log(`* Connected to ${addr}:${port}`);
+    connected = true;
 }
 
 function checkPyramid(msg) {
-    const allEqual = arr => arr.every(val => val === arr[0]);
-
     let seperatedPyramid = msg.split(' ');
     seperatedPyramid = seperatedPyramid.filter((str) => { return /^\w+$/g.test(str); }); // just for safety
     if(seperatedPyramid.length >= 5 && allEqual(seperatedPyramid)) {
@@ -50,7 +100,7 @@ function checkPyramid(msg) {
 async function isStreamLive(target) {
     const ta = new TwitchApi(EnvVars.client_id, EnvVars.client_secret);
     await ta.authorize();
-    let streamInfo = await ta.getStream(target)
+    let streamInfo = await ta.getStream(target);
     await ta.dispose();
     return streamInfo.length != 0;
 }
